e7f645d747046a8b4de12cd599a83b615682431b
GuilhermeManna <57467511+GuilhermeManna@users.noreply.github.com>
2021-10-01 15:16:06 -0300

    Exercício Finalizado

--- exercicio.c
+++ exercicio.c
@@ -4,47 +4,64 @@
 
 #define MAX_P 10
 
-struct Data {
+typedef struct {
   int dia, mes, ano;
-};
+} Data;
 
 struct Pessoa {
   char nome[50];
   char nacionalidade[50];
-  struct Data data;
+  Data data;
   char foto[50];
 } personalidades[MAX_P];
 
 struct Pessoa personalidades[MAX_P] = {
   {"Ada Lovelace","Britânica",{10,12,1815},"foto0.png"},
+  {"Linus Torvalds","Finlandês",{28,12,1969},"foto9.png"},
   {"Grace Hopper","Americana",{9,12,1906},"foto1.png"},
-  {"Alan Turing","Britânico",{23,6,1912},"foto2.png"},
   {"Edgar F. Codd","Britânico",{19,8,1923},"foto3.png"},
-  {"Marvin Minsky","Americano",{9,8,1927},"foto4.png"},
+  {"Alan Turing","Britânico",{23,6,1912},"foto2.png"},
   {"Edsger Dijkstra","Holandês",{11,5,1930},"foto5.png"},
-  {"Donald Knuth","Americano",{10,1,1938},"foto6.png"},
   {"Ward Cunningham","Americano",{26,5,1949},"foto7.png"},
+  {"Donald Knuth","Americano",{10,1,1938},"foto6.png"},
   {"Bill Gates","Americano",{28,10,1955},"foto8.png"},
-  {"Linus Torvalds","Finlandês",{28,12,1969},"foto9.png"}
+  {"Marvin Minsky","Americano",{9,8,1927},"foto4.png"}
 };
 
-int previsaoAno(int *x);
+void ordenar(void);
+int previsaoAno(void);
 
-int previsaoAno(int *x) {
+void ordenar (void)
+{
+  int i, j;
+  for(i = 0; i < MAX_P; ++i)
+  {
+    for(j = i; j < MAX_P; ++j)
+    {
+      if(personalidades[i].data.ano > personalidades[j].data.ano) {
+        struct Pessoa troca = personalidades[i];
+        personalidades[i] = personalidades[j];
+        personalidades[j] = troca;
+      }
+    }
+  }
+}
+
+int previsaoAno(void) {
   int media, difDatas = 0;
 
   for (int i = 0; i < MAX_P; ++i) {
     difDatas += labs(personalidades[i].data.ano - personalidades[i+1].data.ano);
   }
 
-  media = difDatas/10;
+  media = difDatas / MAX_P;
   return personalidades[0].data.ano + media;
 }
 
 int main() {
-  int proximoAno;
+  ordenar();
 
-  printf("A previsão do ano de nascimento da décima-primeira personalidade é: %d\n", previsaoAno(&proximoAno));
+  printf("A previsão do ano de nascimento da décima-primeira personalidade é: %d\n", previsaoAno());
 
   return 0;
 }
